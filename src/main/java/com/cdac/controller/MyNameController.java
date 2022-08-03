4a8c991de3e58b8159bb3e825b72bac3e295899a
khaliddhalait <khaliddhalaitkd@gmail.com>
2022-08-03 11:09:22 +0530

    commited

---
+++ src/main/java/com/cdac/controller/MyNameController.java
@@ -0,0 +1,18 @@
+package com.cdac.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/display")
+public class MyNameController {
+	@GetMapping("/name")
+	public String getEmployee() {
+		String str = "my name is khalid";	
+		return str;
+	}
+}
